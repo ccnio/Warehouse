babe0759675d4373ce766edcba3e0bc58e06a6d3
jianfeng.li <jianfeng.li@moji.com>
2017-12-29 18:31:46 +0800

    init

---
+++ app/src/main/java/com/edreamoon/stu/MApp.java
@@ -0,0 +1,15 @@
+package com.edreamoon.stu;
+
+import android.app.Application;
+
+/**
+ * Created by jianfeng.li on 2017/12/29.
+ */
+
+public class MApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
