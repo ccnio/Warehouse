9b0aa451352a9304fbf679692b077a5d58a33510
jianfeng.li <jianfeng.li@moji.com>
2018-03-13 09:52:04 +0800

    flavor sourcesets; flavor dependence

---
+++ dylib/src/main/java/com/edreamoon/dylib/DyLib.java
@@ -0,0 +1,16 @@
+package com.edreamoon.dylib;
+
+import android.util.Log;
+
+import com.qq.e.ads.nativ.NativeADDataRef;
+
+/**
+ * Created by jianfeng.li on 2018/3/12.
+ */
+
+public class DyLib {
+    public static void test() {
+        NativeADDataRef nav;
+        Log.e("lijf", "test: dev");
+    }
+}
