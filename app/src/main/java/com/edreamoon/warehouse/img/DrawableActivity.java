10f15f8f3d4506d6afd4b5f9ef2169668ca58ec2
jianfeng.li <jianfeng.li@moji.com>
2018-04-27 10:01:18 +0800

    shape

---
+++ app/src/main/java/com/edreamoon/warehouse/img/DrawableActivity.java
@@ -0,0 +1,15 @@
+package com.edreamoon.warehouse.img;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.edreamoon.warehouse.R;
+
+public class DrawableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawable);
+    }
+}
