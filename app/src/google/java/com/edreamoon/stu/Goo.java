9ec268ed1f1390f1e2f7d9cc09d216f0b645aadb
lijianfeng1 <lijianfeng1@xiaomi.com>
2019-05-30 15:20:12 +0800

    remove file

--- app/src/google/java/com/edreamoon/stu/Goo.java
+++
@@ -1,8 +0,0 @@
-package com.ware;
-
-/**
- * Created by jianfeng.li on 2018/3/12.
- */
-
-public class Goo {
-}
--- app/src/main/java/com/ware/Main2Activity.java
+++ app/src/main/java/com/ware/Main2Activity.java
@@ -1,10 +1,16 @@
 package com.ware;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
 
-import com.ware.R;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jianfeng.li on 2017/11/24.
@@ -18,8 +24,64 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mRecyclerView = findViewById(R.id.mRecyclerView);
+
+        findViewById(R.id.bt1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getPackageList();
+            }
+        });
+    }
+
+    private void getPackageList() {
+        long start = System.currentTimeMillis();
+        ArrayList<AppInfo> appList = new ArrayList<>(); //用来存储获取的应用信息数据
+        List<PackageInfo> packages = getPackageManager().getInstalledPackages(0);
+
+        for (int i = 0; i < packages.size(); i++) {
+
+            PackageInfo info = packages.get(i);
+
+            AppInfo tmpInfo = new AppInfo();
+
+            tmpInfo.appName = info.applicationInfo.loadLabel(getPackageManager()).toString();
+
+            tmpInfo.packageName = info.packageName;
+
+            tmpInfo.versionName = info.versionName;
+
+            tmpInfo.versionCode = info.versionCode;
+
+            tmpInfo.appIcon = info.applicationInfo.loadIcon(getPackageManager());
+
+            appList.add(tmpInfo);
+
+
+            if ((info.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
+                Log.d(TAG, "*************************");
+                Log.d(TAG, "Name:" + tmpInfo.appName + " drawable:" + tmpInfo.appIcon);
+                Log.d(TAG, "userid:" + info.sharedUserId + " flags: ");
+            }
+//            if ((info.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0){// 非系统应用
+//                tmpInfo.print();
+//            } else {
+//
+//
+        }
+        Log.d(TAG, "getPackageList: " + (System.currentTimeMillis() - start));
 
     }
 
+
+    public class AppInfo {
+        public String appName = "";
+        public String packageName = "";
+        public String versionName = "";
+        public int versionCode = 0;
+        public Drawable appIcon = null;
+
+        public void print() {
+            Log.v(TAG, "Name:" + appName + " Package:" + packageName + " drawable: " + appIcon);
+        }
+    }
 }
