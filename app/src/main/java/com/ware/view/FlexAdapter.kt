c2e013cf374d0b7d21c622aa1af318bb215c31b0
lijianfeng1 <lijianfeng1@xiaomi.com>
2019-06-20 10:29:42 +0800

    edit scroll

--- app/src/main/java/com/ware/view/FlexAdapter.kt
+++ app/src/main/java/com/ware/view/FlexAdapter.kt
@@ -10,10 +10,16 @@ import com.ware.R
 /**
  * Created by jianfeng.li on 19-6-14.
  */
-class FlexAdapter() : RecyclerView.Adapter<FlexAdapter.FlexHolder>() {
+class FlexAdapter : RecyclerView.Adapter<FlexAdapter.FlexHolder>(), View.OnClickListener {
+    override fun onClick(v: View) {
+//        val tag = v.tag
+        v.isSelected = !(v.isSelected)
+    }
+
     private val mList = mutableListOf("硬件连接", "产品优化", "功能优化", "门禁相关", "跑步相关", "锻炼相关", "其它类型")
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FlexHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.flex_item, parent, false)
+        view.setOnClickListener(this)
         return FlexHolder(view)
     }
 
