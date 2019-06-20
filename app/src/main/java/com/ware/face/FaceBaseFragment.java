10d2aa4c830d2b0971143fd2826f2153f0cb3417
lijianfeng1 <lijianfeng1@xiaomi.com>
2019-06-20 10:36:02 +0800

    remove butter

--- app/src/main/java/com/ware/face/FaceBaseFragment.java
+++ app/src/main/java/com/ware/face/FaceBaseFragment.java
@@ -5,17 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ware.R;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 /**
  * Created by jianfeng.li on 19-6-5.
@@ -33,7 +29,6 @@ public class FaceBaseFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_face_list, container, false);
-        ButterKnife.bind(this, view);
 
         initView(view);
         return view;
--- app/src/main/java/com/ware/face/FaceHolder.java
+++ app/src/main/java/com/ware/face/FaceHolder.java
@@ -8,26 +8,18 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.ware.R;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
-
 /**
  * Created by jianfeng.li on 19-6-5.
  */
 public class FaceHolder extends RecyclerView.ViewHolder {
-    @BindView(R.id.mImageView)
     FaceImageView mImageView;
-    @BindView(R.id.mNameView)
     TextView mNameView;
-    @BindView(R.id.mStateView)
     TextView mStatView;
     public FaceHolder(@NonNull View itemView) {
         super(itemView);
         mImageView = itemView.findViewById(R.id.mImageView);
         mNameView = itemView.findViewById(R.id.mNameView);
         mStatView = itemView.findViewById(R.id.mStateView);
-        ButterKnife.bind(this, itemView);
 //        new Circle
     }
 }
