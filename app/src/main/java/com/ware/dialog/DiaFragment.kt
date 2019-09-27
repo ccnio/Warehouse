721b50023d9c5b8fabf7fd39f6da0380785f0efd
lijianfeng1 <lijianfeng1@xiaomi.com>
2019-09-27 17:55:53 +0800

    gl

--- app/src/main/java/com/ware/dialog/DiaFragment.kt
+++ app/src/main/java/com/ware/dialog/DiaFragment.kt
@@ -1,14 +1,15 @@
 package com.ware.dialog
 
-import android.app.DialogFragment
 import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 
 import com.ware.R
+
 //import com.ware.img.then
 //import com.ware.img.thus
 
@@ -20,8 +21,7 @@ class DiaFragment : DialogFragment() {
     /**
      * 如屏幕旋转时onCreateView, onCreate等会被调用，edittext内容会被清空。而普通的Dialog则直接消失不会重建
      */
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle): View? {
-        Log.e("lijf", "onCreateView: ")
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.layout_dia_fragment, container)
     }
 
---
+++ app/src/main/java/com/ware/view/gl/GlTestActivity.java
@@ -0,0 +1,110 @@
+package com.ware.view.gl;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ware.R;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class GlTestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GLSurfaceView glSurfaceView = findViewById(R.id.mGLSurfaceView);
+        glSurfaceView.setEGLContextClientVersion(2);
+        glSurfaceView.setRenderer(new MyRenderer());
+        glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+    }
+
+    static class MyRenderer implements GLSurfaceView.Renderer {
+        private static final String VERTEX_SHADER = "attribute vec4 vPosition;\n"
+                + "uniform mat4 uMVPMatrix;\n"
+                + "void main() {\n"
+                + "  gl_Position = uMVPMatrix * vPosition;\n"
+                + "}";
+        private static final String FRAGMENT_SHADER = "precision mediump float;\n"
+                + "void main() {\n"
+                + "  gl_FragColor = vec4(0.5,0,0,1);\n"
+                + "}";
+        private static final float[] VERTEX = {   // in counterclockwise order:
+                0, 1, 0.0f, // top
+                -0.5f, -1, 0.0f, // bottom left
+                1f, -1, 0.0f,  // bottom right
+        };
+
+        private final FloatBuffer mVertexBuffer;
+        private final float[] mProjectionMatrix = new float[16];
+        private final float[] mCameraMatrix = new float[16];
+        private final float[] mMVPMatrix = new float[16];
+
+        private int mProgram;
+        private int mPositionHandle;
+        private int mMatrixHandle;
+
+        MyRenderer() {
+            mVertexBuffer = ByteBuffer.allocateDirect(VERTEX.length * 4)
+                    .order(ByteOrder.nativeOrder())
+                    .asFloatBuffer()
+                    .put(VERTEX);
+            mVertexBuffer.position(0);
+        }
+
+        static int loadShader(int type, String shaderCode) {
+            int shader = GLES20.glCreateShader(type);
+            GLES20.glShaderSource(shader, shaderCode);
+            GLES20.glCompileShader(shader);
+            return shader;
+        }
+
+        @Override
+        public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+        }
+
+        @Override
+        public void onSurfaceChanged(GL10 unused, int width, int height) {
+            mProgram = GLES20.glCreateProgram();
+            int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, VERTEX_SHADER);
+            int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, FRAGMENT_SHADER);
+            GLES20.glAttachShader(mProgram, vertexShader);
+            GLES20.glAttachShader(mProgram, fragmentShader);
+            GLES20.glLinkProgram(mProgram);
+
+            mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+            mMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+
+            float ratio = (float) height / width;
+            Matrix.frustumM(mProjectionMatrix, 0, -1, 1, -ratio, ratio, 3, 7);
+            Matrix.setLookAtM(mCameraMatrix, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0);
+            Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mCameraMatrix, 0);
+        }
+
+        @Override
+        public void onDrawFrame(GL10 unused) {
+            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+
+            GLES20.glUseProgram(mProgram);
+
+            GLES20.glEnableVertexAttribArray(mPositionHandle);
+            GLES20.glVertexAttribPointer(mPositionHandle, 3, GLES20.GL_FLOAT, false, 12,
+                    mVertexBuffer);
+
+            GLES20.glUniformMatrix4fv(mMatrixHandle, 1, false, mMVPMatrix, 0);
+
+            GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
+
+            GLES20.glDisableVertexAttribArray(mPositionHandle);
+        }
+    }
+}
