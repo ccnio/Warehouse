ccdfae6e78b87de3fdbe05c30524c911abe72909
lijianfeng <lijianfeng1@xiaomi.com>
2020-03-30 11:22:24 +0800

    Activity Result API

--- app/src/main/java/com/ware/component/AcActivity.kt
+++ app/src/main/java/com/ware/component/AcActivity.kt
@@ -5,8 +5,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.ware.R
-import com.ware.common.BaseActivity
 import com.ware.kt.KtActivity
 import com.ware.systip.SecActivity
 import kotlinx.android.synthetic.main.activity_launch_mode.*
@@ -16,12 +18,12 @@ import kotlinx.android.synthetic.main.activity_launch_mode.*
 例子是在一个activity里启动自己，查看生命周期的调用。
 - standard: 每次都会调用onCreate(),并且任务栈里有多个此activity的实例
 - singleTop:每次调用startActivity()不会再创建activity实例，生命周期也不会回调，会回调onNewIntent()
-
 ## 后台startActivity的操作都将会延迟几秒
-
 ## Q及后　在后台启动Activity的限制
+# ActivityResultContract--替代startActivityForResult: https://juejin.im/post/5e80cb1ee51d45471654fae7
+新的 Activity Result API，我们还可以单独的类中处理结果回调，真正做到 单一职责 。通过 ActivityResultRegistry 来完成的，ComponentActivity 中包含了一个 ActivityResultRegistry 对象
  */
-class AcActivity : BaseActivity(), View.OnClickListener {
+class AcActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View) {
         when (v.id) {
@@ -34,10 +36,23 @@ class AcActivity : BaseActivity(), View.OnClickListener {
             R.id.mWakeLockView -> {
                 mWakeLockView.postDelayed({ com.ware.systip.start() }, 1000 * 10)
             }
-            R.id.mInfoView -> {
-                SecActivity.start(this)
+            R.id.goSecView -> {
+                startForRet()
             }
         }
+    }
+
+    /**
+     * 除了 StartActivityForResult 之外，官方还默认提供了 RequestPermissions ，Dial ，RequestPermission ，TakePicture ，它们都是 ActivityResultContract 的实现类。
+     * 除了使用官方默认提供的这些之外，我们还可以自己实现 ActivityResultContract。
+     */
+    private val launcher: ActivityResultLauncher<Intent> = prepareCall(ActivityResultContracts.StartActivityForResult()) { activityResult ->
+        Log.d("AcActivity", activityResult.toString())//ActivityResult{resultCode=RESULT_CANCELED, data=null}
+    }
+
+    private fun startForRet() {
+        val intent = Intent(this, SecActivity::class.java)
+        launcher.launch(intent)
     }
 
     private fun startActivityBackground() {
@@ -53,7 +68,7 @@ class AcActivity : BaseActivity(), View.OnClickListener {
         mLaunchModeView.setOnClickListener(this)
         mBgAcView.setOnClickListener(this)
         mWakeLockView.setOnClickListener(this)
-        mInfoView.setOnClickListener(this)
+        goSecView.setOnClickListener(this)
         Log.d("AcActivity", "onCreate: ")
     }
 
