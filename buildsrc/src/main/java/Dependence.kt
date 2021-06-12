afd98bd06323c2a48427a65483fa300063a59643
lijf <lijianfeng1@xiaomi.com>
2021-06-13 07:56:57 +0800

    依赖管理

---
+++ buildsrc/src/main/java/Dependence.kt
@@ -0,0 +1,13 @@
+object Versions {
+    const val multidex_version = "2.0.1"
+    const val paging_runtime_ktx = "2.1.2"
+}
+
+object Config {
+    const val applicationId = "com.ware"
+}
+
+object Libs {
+    const val multidex = "androidx.multidex:multidex:${Versions.multidex_version}"
+    const val paging_runtime_ktx = "androidx.paging:paging-runtime-ktx:2.1.2"
+}
