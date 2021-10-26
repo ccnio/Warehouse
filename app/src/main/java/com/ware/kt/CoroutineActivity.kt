129d79def844396d2a1705535a3997ba7ed5a04c
lijianfeng1 <lijianfeng1@xiaomi.com>
2021-10-26 15:23:21 +0800

    callback to coroutine

--- app/src/main/java/com/ware/kt/CoroutineActivity.kt
+++ app/src/main/java/com/ware/kt/CoroutineActivity.kt
@@ -10,7 +10,16 @@ import com.ware.jetpack.viewbinding.viewBinding
 import kotlinx.android.synthetic.main.activity_coroutine.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.conflate
+import kotlin.concurrent.thread
 import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 import kotlin.system.measureTimeMillis
 
 private const val TAG_L = "CoroutineActivityL"
@@ -19,9 +28,10 @@ private const val TAG_L = "CoroutineActivityL"
  * - delay/ SystemClock.sleep 区别: delay 是suspend函数 执行时会挂起所在协程,时间到后再恢复,不会阻塞其它线程. sleep 休眠当前线程,会阻塞当前线程.
  * - 协程可以延迟启动
  * - GlobalScope.launch 默认在 Dispatchers.Default 上执行
+ * # 协程里创建一个作用域，并在此作用域开启协程，此作用域上的协程与外面的协程并行执行[suspendCancelableCallback]
  *
  */
-class CoroutineActivity : AppCompatActivity(), View.OnClickListener, CoroutineScope { //by MainScope() way 4
+class CoroutineActivity : AppCompatActivity(), View.OnClickListener, CoroutineScope by MainScope() { // way 4
     private val binding by viewBinding(ActivityCoroutineBinding::bind)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,9 +39,9 @@ class CoroutineActivity : AppCompatActivity(), View.OnClickListener, CoroutineSc
         setContentView(R.layout.activity_coroutine)
         coroutineJob = Job()
 
+        binding.notBlockView.setOnClickListener { notBlock() }
         binding.scopeView.setOnClickListener(this)
         binding.blockingView.setOnClickListener(this)
-        binding.notBlockView.setOnClickListener(this)
         binding.launchView.setOnClickListener(this)
         binding.nestCoroutineView.setOnClickListener(this)
         binding.asyncMulTaskView.setOnClickListener(this)
@@ -39,13 +49,201 @@ class CoroutineActivity : AppCompatActivity(), View.OnClickListener, CoroutineSc
         tv4.setOnClickListener(this)
         tv5.setOnClickListener(this)
         tv6.setOnClickListener(this)
+        binding.callbackView.setOnClickListener { callback() }
+    }
+
+    /**
+     * callback 1
+     */
+    interface SingleMethodCallback {
+        fun onCallback(ret: String)
+    }
+
+    private fun runTask(callback: SingleMethodCallback) {
+        thread {
+            Thread.sleep(2000)//task
+            callback.onCallback("ret")
+        }
+    }
+
+    private suspend fun callback1Suspend() = suspendCoroutine<String> { continuation ->
+        runTask(object : SingleMethodCallback {
+            override fun onCallback(ret: String) {
+                Log.d(TAG_L, "callback1Suspend: $ret ${Thread.currentThread().name}")
+                continuation.resume(ret)
+            }
+        })
+    }
+
+    /**
+     * callback 2
+     */
+    interface SuccessOrFailCallback {
+        fun onSuccess(ret: String)
+        fun onFail(code: Throwable)
+    }
+
+    private fun runTask2(callback: SuccessOrFailCallback) {
+        thread {
+            try {
+                Thread.sleep(2000)
+                callback.onSuccess("ret2")
+            } catch (e: Throwable) {
+                callback.onFail(e)
+            }
+        }
+    }
+
+    private suspend fun callback2Suspend() = suspendCoroutine<String> { continuation ->
+        runTask2(object : SuccessOrFailCallback {
+            override fun onSuccess(ret: String) {
+                Log.d(TAG_L, "callback2Suspend: success ${Thread.currentThread().name}")
+                continuation.resume(ret)
+            }
+
+            override fun onFail(throwable: Throwable) {
+                continuation.resumeWithException(throwable)
+            }
+        })
+    }
+
+    /**
+     * callback 3: cancelable
+     */
+
+    interface Cancelable {
+        fun cancel()
+    }
+
+    private fun taskCancelable(callback: SuccessOrFailCallback): Cancelable {
+        val t = thread {
+            try {
+                Thread.sleep(1000)
+                callback.onSuccess("ret3")
+            } catch (e: Exception) {
+                callback.onFail(e)
+            }
+        }
+        return object : Cancelable {
+            override fun cancel() {
+                t.interrupt() //调用时会导致 sleep 抛出异常
+            }
+        }
+    }
+
+    private suspend fun suspendCancelableCallback() = suspendCancellableCoroutine<String> { cancellableContinuation ->
+        val taskCancelable = taskCancelable(object : SuccessOrFailCallback {
+            override fun onSuccess(ret: String) {
+                cancellableContinuation.resume(ret)
+            }
+
+            override fun onFail(code: Throwable) {
+                cancellableContinuation.resumeWithException(code)
+            }
+        })
+        cancellableContinuation.invokeOnCancellation { taskCancelable.cancel() }
+    }
+
+    /**
+     * callback 4: callbackFlow
+     */
+    interface MultiCallback<T> {
+        fun onProgress(progress: Int)
+        fun onResult(ret: T)
+        fun onError(e: Throwable)
+        fun onComplete()
+    }
+
+    private fun task4(callback: MultiCallback<String>): Cancelable {
+        val t = thread {
+            try {
+                (0..100).forEach {
+                    Thread.sleep(50)
+                    callback.onProgress(it)
+                }
+                callback.onResult("Done")
+                callback.onComplete()
+            } catch (e: Exception) {
+                callback.onError(e)
+            }
+        }
+        return object : Cancelable {
+            override fun cancel() {
+                t.interrupt()
+            }
+        }
+    }
+
+    sealed interface Event
+    class OnProgress(val progress: Int) : Event
+    class OnError(val t: Throwable) : Event
+    class OnResult(val ret: String) : Event
+    object OnComplete : Event
+
+    private suspend fun suspendCallbackFlow() = callbackFlow { // 或者 channel flow
+        val task4 = task4(object : MultiCallback<String> {
+            override fun onProgress(progress: Int) {
+                trySendBlocking(OnProgress(progress))
+            }
+
+            override fun onResult(ret: String) {
+                trySendBlocking(OnResult(ret))
+            }
+
+            override fun onError(e: Throwable) {
+                trySendBlocking(OnError(e))
+                close()
+            }
+
+            override fun onComplete() {
+                trySendBlocking(OnComplete)
+                close()
+            }
+        })
+        awaitClose { //没有关闭 或者 外部取消，会一直挂起，所以注意 close/cancel
+            task4.cancel()
+        }
+    }.conflate() //如果数据比较密集，只用最后一次的
+
+    private fun callback() {
+        launch {
+            val ret1 = callback1Suspend()
+            Log.d(TAG_L, "callback: $ret1 ${Thread.currentThread().name}")
+
+            val ret2 = callback2Suspend()
+            Log.d(TAG_L, "callback2: $ret2 ${Thread.currentThread().name}")
+
+            val scope = CoroutineScope(Dispatchers.Main) //
+            scope.launch {
+                try {
+                    val suspendCancelableCallback = suspendCancelableCallback()
+                    Log.d(TAG_L, "callback3: $suspendCancelableCallback; ${Thread.currentThread().name}")
+                } catch (e: Exception) {
+                    Log.d(TAG_L, "callback3: exception = ${e.message}; ${Thread.currentThread().name}")
+                }
+            }
+//            delay(200)
+//            scope.cancel()
+
+            scope.launch {
+                suspendCallbackFlow().collect {
+                    when (it) {
+                        OnComplete -> Log.d(TAG_L, "callback: OnComplete")
+                        is OnError -> Log.d(TAG_L, "callback: error ${it.t}")
+                        is OnProgress -> Log.d(TAG_L, "callback: progress ${it.progress}")
+                        is OnResult -> Log.d(TAG_L, "callback: result ${it.ret}")
+                    }
+                }
+            }
+            delay(800)
+            scope.cancel()
+        }
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.scope_view -> scope()
             R.id.blocking_view -> blocking()
-            R.id.not_block_view -> notBlock()
             R.id.launch_view -> launchView()
             R.id.nest_coroutine_view -> nestCoroutine()
             R.id.asyncMulTaskView -> mulTaskSameTime()
