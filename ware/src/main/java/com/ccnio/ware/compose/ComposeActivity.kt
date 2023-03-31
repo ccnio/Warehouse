6ba04fd178e3590afac5218d0d9fa676f96516f2
lijianfeng03 <lijianfeng03@zuoyebang.com>
2023-03-31 19:20:30 +0800

    version

--- ware/src/main/java/com/ccnio/ware/compose/ComposeActivity.kt
+++ ware/src/main/java/com/ccnio/ware/compose/ComposeActivity.kt
@@ -1,18 +1,32 @@
 package com.ccnio.ware.compose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ccnio.ware.compose.ui.theme.WarehouseTheme
 
+private const val TAG = "ComposeActivity"
+
 class ComposeActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +36,9 @@ class ComposeActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val list = mutableListOf<String>()
+                    for (i in 0..50) list.add("Item $i")
+                    MyList(items = list)
                 }
             }
         }
@@ -30,14 +46,37 @@ class ComposeActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyList(items: List<String>) {
+    val context = LocalContext.current
+    LazyColumn {
+        items(items) { item ->
+            ClickableText(
+                text = AnnotatedString(item),
+                onClick = { Toast.makeText(context, "$item  $context", Toast.LENGTH_SHORT).show() },
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+            )
+
+            Divider(
+                color = Color.Blue,
+                thickness = 1.dp,
+                startIndent = 16.dp,
+                modifier = Modifier.padding(end = 16.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     WarehouseTheme {
-        Greeting("Android")
+        MyList(items = listOf("a", "b", "c"))
     }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
--- ware/src/main/java/com/ccnio/ware/media/ExoPlayerActivity.kt
+++ ware/src/main/java/com/ccnio/ware/media/ExoPlayerActivity.kt
@@ -1,44 +1,26 @@
 package com.ccnio.ware.media
 
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.ccnio.ware.R
+import com.ccnio.ware.databinding.ActivityExoplayerBinding
+import com.ccnio.ware.jetpack.viewbinding.viewBinding
 import com.google.android.exoplayer2.*
-import com.google.android.exoplayer2.analytics.AnalyticsListener
-import com.google.android.exoplayer2.database.ExoDatabaseProvider
-import com.google.android.exoplayer2.source.LoadEventInfo
-import com.google.android.exoplayer2.source.MediaLoadData
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
-import com.google.android.exoplayer2.ui.PlayerView
-import com.google.android.exoplayer2.upstream.cache.LeastRecentlyUsedCacheEvictor
-import com.google.android.exoplayer2.upstream.cache.SimpleCache
-import java.io.File
 
 
 private const val TAG = "ExoPlayerLog"
 
-class ExoPlayerActivity : AppCompatActivity(), View.OnClickListener {
-    private val simpleCache by lazy {
-        SimpleCache(
-            File(getExternalFilesDir(null)!!.absolutePath),
-            LeastRecentlyUsedCacheEvictor(1024 * 1024 * 600),
-            ExoDatabaseProvider(this),
-            null,
-            false,
-            false
-        )
-    } //全局配置: 应该使用单例
-    private val trackSelector by lazy { DefaultTrackSelector(this) }
+class ExoPlayerActivity : AppCompatActivity() {
+    private val bind by viewBinding(ActivityExoplayerBinding::bind)
+    private val player by lazy { ExoPlayer.Builder(this).build() }
 
-    private lateinit var player: ExoPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exoplayer)
-        val videoUri = "https://v-cdn.zjol.com.cn/276982.mp4"
+//        val videoUri = "https://v-cdn.zjol.com.cn/276982.mp4"
+        val videoUri = "http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/gear3/prog_index.m3u8"
         val mediaItem: MediaItem = MediaItem.fromUri(videoUri)  // Build the media item.
-        player = ExoPlayer.Builder(this).build()
+
         player.setMediaItem(mediaItem)  // Set the media item to be played.
 
         /*   //1. surface view
@@ -46,70 +28,9 @@ class ExoPlayerActivity : AppCompatActivity(), View.OnClickListener {
            player.setVideoSurfaceView(playerView)*/
 
         //2. player view
-        val playerView: PlayerView = findViewById(R.id.player_view)
-        playerView.player = player
+        bind.playerView.player = player
 
-        //        setResolution()
-        //val dataSourceFactory: DataSource.Factory =
-        //   DefaultHttpDataSource.Factory()// Create a data source factory.
-        //        val cacheDataSourceFactory: DataSource.Factory =
-        //            CacheDataSource.Factory().setUpstreamDataSourceFactory(dataSourceFactory) //cache
-
-        player.addAnalyticsListener(DetailListener())
-        player.addListener(PlayerListener())
         player.prepare() // Prepare the player.
         player.play() // Start the playback.
-    }
-
-    private inner class DetailListener : AnalyticsListener {
-
-    }
-
-
-    private inner class PlayerListener : Player.Listener {
-        override fun onPlaybackStateChanged(playbackState: Int) {
-            val stateString: String = when (playbackState) {
-                ExoPlayer.STATE_IDLE -> "ExoPlayer.STATE_IDLE      -"
-                ExoPlayer.STATE_BUFFERING -> {
-                    "ExoPlayer.STATE_BUFFERING -"
-                }
-                ExoPlayer.STATE_READY -> "ExoPlayer.STATE_READY     -"
-                ExoPlayer.STATE_ENDED -> "ExoPlayer.STATE_ENDED     -"
-                else -> "UNKNOWN_STATE             -"
-            }
-            Log.d(TAG, "changed state to $stateString")
-        }
-
-        override fun onEvents(player: Player, events: Player.Events) {
-            Log.d(TAG, "onEvents: $events")
-        }
-    }
-
-
-    private val BUFFER_TRACK = 1
-    private val BUFFER_SEEK = 2
-    private val BUFFER_ELSE = 3
-    private var bufferReason = BUFFER_ELSE
-
-    private var resolutionFlag = true
-    private fun setResolution() {
-        val width = if (resolutionFlag) 1920 else 1280 //onVideoSizeChanged: width = 1920; h = 1080
-        val height = if (resolutionFlag) 1080 else 720 //onVideoSizeChanged: width = 1280; h = 720
-        trackSelector.parameters =
-            trackSelector.parameters.buildUpon().setMaxVideoSize(width, height)
-                .setMinVideoSize(width, height)
-                .build()//.setMaxVideoBitrate(14442347) //14442347 16361812 42324493
-        resolutionFlag = !resolutionFlag
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        simpleCache.release()
-        player.release()
-    }
-
-    override fun onClick(v: View) {
-        when (v.id) {
-        }
     }
 }
